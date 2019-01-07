95d129e4f07ee3a958331b5374580de02cf702cb
Georgi Angelov <the.wizarda@gmail.com>
2019-01-07 15:15:21 +0200

    Update arduino_bc66.py

--- builder/frameworks/arduino_bc66.py
+++ builder/frameworks/arduino_bc66.py
@@ -6,11 +6,14 @@ import sys, os
 from os.path import join
 from SCons.Script import ARGUMENTS, DefaultEnvironment, Builder
 from bin_bc66 import makeHDR, makeCFG
+from pyMT2625 import bc66_upload
+
+def bc66_uploader(target, source, env):
+    return bc66_upload(join(env.get("BUILD_DIR"), "program.bin"), env.get("UPLOAD_PORT")) 
 
 def bc66_header(target, source, env):
     makeHDR( source[0].path )
     makeCFG( source[0].path )
-
 
 def bc66_init(env):
     TOOL_DIR = env.PioPlatform().get_package_dir("tool-quectel")
@@ -96,11 +99,16 @@ def bc66_init(env):
                 suffix = ".bin"
             )        
         ), # dict
-
-        UPLOADER = join(TOOL_DIR, CORE, UPL, "coda"), 
-        UPLOADERFLAGS = [ '"$BUILD_DIR/${PROGNAME}.cfg"', "--UART", "$UPLOAD_PORT", "-d" ],
-        UPLOADCMD = '"$UPLOADER" $UPLOADERFLAGS',
-
+        
+### WINDOWS UPLOADER ###
+        #UPLOADER = join(TOOL_DIR, CORE, UPL, "coda"), 
+        #UPLOADERFLAGS = [ '"$BUILD_DIR/${PROGNAME}.cfg"', "--UART", "$UPLOAD_PORT", "-d" ],
+        #UPLOADCMD = '"$UPLOADER" $UPLOADERFLAGS',
+### PYTHON UPLOADER ###
+        UPLOADER = '"$PYTHONEXE"',   
+        UPLOADERFLAGS = [ '"' + join(DIR, "frameworks", "bc66_uploader.py") + '"', '"$BUILD_DIR/${PROGNAME}.bin"', "$UPLOAD_PORT" ],
+        #UPLOADCMD = '"$UPLOADER" $UPLOADERFLAGS'
+        UPLOADCMD = bc66_uploader
     ) # env.Append    
 
     libs = []
