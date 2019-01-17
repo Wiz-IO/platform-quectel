dadf935fbeb0638934c9b23b407e49e3e897775f
Georgi Angelov <the.wizarda@gmail.com>
2019-01-17 15:44:52 +0200

    Update opencpu_m66.py

--- builder/frameworks/opencpu_m66.py
+++ builder/frameworks/opencpu_m66.py
@@ -20,7 +20,7 @@ def m66_init(env):
     CORE_DIR = join(env.PioPlatform().get_package_dir("framework-quectel"), "opencpu", CORE)    
 
     env.Append(
-        CPPDEFINES=[ "CORE_"+CORE.upper() ], # -D
+        CPPDEFINES=["OPENCPU", "CORE_"+CORE.upper()], # -D
         CPPPATH=[ # -I
             CORE_DIR,
             join(CORE_DIR, "include"),
