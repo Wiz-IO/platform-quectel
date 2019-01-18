b008de1a4ee246f628eba4e7ca1bfd9c9e15745c
Georgi Angelov <the.wizarda@gmail.com>
2019-01-18 12:44:24 +0200

    Update arduino_bg96.py

--- builder/frameworks/arduino_bg96.py
+++ builder/frameworks/arduino_bg96.py
@@ -30,5 +30,38 @@ def bg96_compiler(env):
     )    
     
 def bg96_init(env):
+    print("\n\033[31mFramework is in development..." )
+    exit(2)
     bg96_compiler(env)
-
+    VARIANT = env.BoardConfig().get("build.variant")
+    CORE = env.BoardConfig().get("build.core") # "bg96"
+    CORE_DIR = join(env.PioPlatform().get_package_dir("framework-quectel"), "arduino", "cores", CORE)   
+    env.Replace(PROGNAME="arduino", PROGSUFFIX='')
+    env.Append(
+        BUILDERS=dict(
+            ElfToBin=Builder(
+                action=env.VerboseAction(" ".join([
+                    "$OBJCOPY",
+                    "-O", 
+                    "binary",
+                    "$SOURCES",
+                    "$TARGET"
+                ]), "Building $TARGET"),
+                suffix=".dat"
+            ),     
+            MakeHeader = Builder(action="", suffix=".2")       
+        ), #dict
+        UPLOADCMD = bg96_uploader         
+    )    
+    libs = []
+    libs.append(
+        env.BuildLibrary(
+            join("$BUILD_DIR", "framework"),
+            join(CORE_DIR)
+    ))
+    libs.append(
+        env.BuildLibrary(
+            join("$BUILD_DIR", "framework", "interface"),
+            join(CORE_DIR, "interface")
+    ))
+    env.Append( LIBS=libs )    
