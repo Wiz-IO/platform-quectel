e3b21dd361f10216fee594d10f30b5a16eddb96d
Georgi Angelov <the.wizarda@gmail.com>
2020-11-26 13:01:35 +0200

    issue #40

--- builder/frameworks/mc60.py
+++ builder/frameworks/mc60.py
@@ -10,6 +10,9 @@ from os.path import join
 
 def makeHDR( dat ):   
     bin = dat.replace(".dat", ".bin")
+    dat_size = os.stat( dat ).st_size
+    rem_size = 16 - dat_size % 16
+    dat_size += rem_size
 
     dst = open(bin, "wb")
     arr = [0x4D, 0x4D, 0x4D, 0x01, 0x40, 0x00, 0x00, 0x00, 0x46, 0x49, 0x4C, 0x45, 0x5F, 0x49, 0x4E, 0x46]
@@ -17,11 +20,8 @@ def makeHDR( dat ):
     dst.write(data) 
     arr = [0x4F, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x70, 0x07, 0x00, 0x00, 0xB0, 0x2D, 0x10] #MC60
     data = bytearray(arr)
-    dst.write(data) 
-    
-    src_size = os.stat( dat ).st_size + 64 #+64
-    #print( "APPLICATIN SIZE: ", src_size + 64, " bytes" )
-    dst.write( struct.pack('<i', src_size) ) # write size 
+    dst.write(data)     
+    dst.write( struct.pack('<i', dat_size + 64) ) # write size 
 
     arr = [                        0xFF, 0xFF, 0xFF, 0xFF, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     data = bytearray(arr)
@@ -32,6 +32,9 @@ def makeHDR( dat ):
 
     src = open(dat, "rb")
     dst.write( src.read() )
+    for i in range(rem_size):
+        dst.write(b"\0")
+    print( "BIN SIZE:", dst.tell()/1024, "kB" )
 
     src.close()
     dst.close()  
